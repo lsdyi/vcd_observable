4dde3ed0933557a03ea219329b5b092320104257
lsdyi <yangjinzhehao123@outlook.com>
2026-02-07 20:15:23 +0100

    feat: PCA page

--- src/components/r.js
+++ src/components/r.js
@@ -4,6 +4,7 @@ import { modelConfig } from "./modelConfig.js";
 
 const webR = new WebR();
 await webR.init();
+await webR.installPackages(['jsonlite']);
 
 const regressionBy = async (family) => {
   const { continousCovariates, response } = modelConfig;
@@ -29,4 +30,29 @@ const getSummary = async () => {
   return text[0];
 };
 
-export { webR, regressionBy, getSummary };
+async function calculatePCA(jsData) {
+  // 1. Convert your JS data to a JSON string for easy transfer
+  const jsonData = JSON.stringify(jsData);
+
+  // 2. Execute the R code
+  const result = await webR.evalR(`
+    library(jsonlite)
+    data <- fromJSON('${jsonData}')
+    pca_res <- prcomp(data, center = TRUE, scale. = TRUE)
+    
+    # We return a list to capture multiple parts of the analysis
+    list(
+      pc1 = pca_res$x[,1],
+      pc2 = pca_res$x[,2],
+      pc3 = pca_res$x[,3],
+      loadings = pca_res$rotation,
+      variance = (pca_res$sdev^2) / sum(pca_res$sdev^2)
+    )
+  `);
+
+  // 3. Convert the R object back to a JS object
+  const output = await result.toJs();
+  return output;
+}
+
+export { webR, regressionBy, getSummary, calculatePCA };
