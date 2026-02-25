d0b3f361239a8a76f6d0521f78cd3158d6fe18ba
lsdyi <52379902+lsdyi@users.noreply.github.com>
2026-02-25 00:40:45 +0000

    Deploying to docs/building_output from @ lsdyi/vcd_observable@cacc08f9a67a3902f0db43637e798a33907447e4 🚀

---
+++ _import/components/r.a79a2b4b.js
@@ -0,0 +1,48 @@
+import { WebR } from "../../_node/webr@0.5.8/index.d12ac113.js";
+
+import { modelConfig } from "./modelConfig.6feb3b8c.js";
+
+const webR = new WebR();
+await webR.init();
+await webR.installPackages(["betareg"]);
+
+const regressionBy = async (family) => {
+  const { continousCovariates, response } = modelConfig;
+  const modelStr = `${response} ~ ${continousCovariates.join(" + ")}`;
+  await webR.evalR(`
+  df <- as.data.frame(df_raw)
+  model <- glm(${modelStr}, family = ${family}, data = df)
+  summary_stats <- summary(model)
+`);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR("as.data.frame(summary_stats$coefficients)");
+  const output = await result.toJs();
+
+  return output;
+};
+
+const getSummary = async () => {
+  const result = await webR.evalR(
+    'paste(capture.output(summary_stats), collapse = "\n")',
+  );
+  const text = await result.toArray();
+  return text[0];
+};
+
+const betaRegession = async () => {
+  const rCodes = `
+    library(betareg)
+    fit <- betareg(Y ~ X1 + X2 + X3, link = "logit", data = betaData)
+  `;
+
+  await webR.evalR(rCodes);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR("as.data.frame(summary(fit)$coefficients)");
+  const output = await result.toJs();
+
+  return output;
+};
+
+export { webR, regressionBy, getSummary, betaRegession };
