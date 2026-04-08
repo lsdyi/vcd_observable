94392ce9c29eafb73ce6a3d52ef55591472420d8
lsdyi <52379902+lsdyi@users.noreply.github.com>
2026-04-08 01:54:44 +0000

    Deploying to docs/building_output from @ lsdyi/vcd_observable@ac1f6dd23e4d4e3883583150d0a4d75b5cf233ec 🚀

---
+++ _import/components/r.fa3a19ed.js
@@ -0,0 +1,175 @@
+import { WebR } from "../../_node/webr@0.5.8/index.d12ac113.js";
+import jStat from "../../_node/jstat@1.9.6/index.bc60a888.js";
+
+import { modelConfig } from "./modelConfig.6feb3b8c.js";
+
+const webR = new WebR();
+await webR.init();
+await webR.installPackages(["betareg", "MASS", "np"]);
+
+const regressionBy = async (family) => {
+  const { continousCovariates, response } = modelConfig;
+  const modelStr = `${response} ~ ${continousCovariates.join(" + ")}`;
+  await webR.evalR(`
+  df <- as.data.frame(df_raw)
+  model <- glm(${modelStr}, family = ${family}, data = df)
+  summary_stats <- summary(model)
+`);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR("as.data.frame(summary_stats$coefficients)");
+  const output = await result.toJs();
+
+  return output;
+};
+
+const getSummary = async () => {
+  const result = await webR.evalR(
+    'paste(capture.output(summary_stats), collapse = "\n")',
+  );
+  const text = await result.toArray();
+  return text[0];
+};
+
+const betaRegession = async () => {
+  const rCodes = `
+    library(betareg)
+    fit <- betareg(Y ~ X1 + X2 + X3, link = "logit", data = betaData)
+  `;
+
+  await webR.evalR(rCodes);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR("as.data.frame(summary(fit)$coefficients)");
+  const output = await result.toJs();
+
+  return output;
+};
+
+// negative binomial regression
+const negRegession = async (
+  dataName = "poiNegData",
+  setup = "y ~ x1 + x2 + x3",
+) => {
+  const rCodes = `
+    library(MASS)
+    fit <- glm.nb(${setup}, data = ${dataName})
+    summary_stats <- summary(fit)
+  `;
+
+  await webR.evalR(rCodes);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR(`
+c(
+    coef(fit), # regression coefficients
+    fit$theta # dispersion parameter
+)
+    `);
+  const output = await result.toJs();
+
+  return output;
+};
+
+// poisson regression
+const poissonRegession = async (
+  dataName = "poiNegData",
+  setup = "y ~ x1 + x2 + x3",
+) => {
+  const rCodes = `
+    fit <- glm(${setup}, family = poisson, data = ${dataName})
+    summary_stats <- summary(fit)
+  `;
+
+  await webR.evalR(rCodes);
+
+  // Extracting coefficients specifically
+  const result = await webR.evalR(`
+c(
+    coef(fit), # regression coefficients
+    fit$theta # dispersion parameter
+)
+    `);
+  const output = await result.toJs();
+
+  return output;
+};
+
+const getPearsonResiduals = async () => {
+  const rCodes = `
+    p_res <- residuals(fit, type = "pearson")
+  `;
+
+  const result = await webR.evalR(rCodes);
+
+  const output = await result.toJs();
+
+  return output;
+};
+
+const cke = async () => {
+  const rCodes = `
+    library(np)
+    y_count <- ordered(poiNegData['y'])
+    x1 <- poiNegData['x1']
+    x2 <- poiNegData['x2']
+    x3 <- poiNegData['x3']
+    new_data <- data.frame(y_count, x1, x2,x3)
+    
+    bw_cond <- npcdensbw(y_count ~ x1 + x2 + x3,
+    data = new_data,
+    bwmethod = "cv.ml")
+
+    summary_stats <- summary(bw_cond)
+  `;
+
+  await webR.evalR(rCodes);
+
+  const result = await webR.evalR(
+    'paste(capture.output(summary_stats), collapse = "\n")',
+  );
+  const text = await result.toArray();
+  return text[0];
+};
+
+const loess = async () => {
+  const rCodes = `
+    # fit loess model
+    fit <- loess(Y ~ X1 + X2 + X3, data = betaData, span = 0.5)
+    summary_stats <- summary(fit)
+    new_df <- data.frame(
+      X1 = c(0),
+      X2 = c(1),
+      X3 = c(2)
+    )
+
+    mu = predict(fit, newdata = new_df)
+    `;
+  await webR.evalR(rCodes);
+
+  const result = await webR.evalR(`
+    c(
+      mu
+    )
+    `);
+  const output = await result.toJs();
+
+  return output;
+  // const result = await webR.evalR(
+  //   'paste(capture.output(summary_stats), collapse = "\n")',
+  // );
+  // const text = await result.toArray();
+  // return text[0];
+};
+
+export {
+  webR,
+  regressionBy,
+  getSummary,
+  betaRegession,
+  negRegession,
+  poissonRegession,
+  cke,
+  loess,
+  getPearsonResiduals
+};
---
+++ _import/components/scatterPlot3d.95fe92c3.js
@@ -0,0 +1,50 @@
+import Plotly from "../../_node/plotly.js-dist-min@3.5.0/index.96ab480f.js";
+
+function unpack(rows, key) {
+  return rows.map(function (row) {
+    return row[key];
+  });
+}
+
+export const scatterPlot3d = (data, keys = ["x", "y", "z"], cordinate = []) => {
+  const trace = [
+    {
+      x: unpack(data, keys[0]),
+      y: unpack(data, keys[1]),
+      z: unpack(data, keys[2]),
+      mode: "markers",
+      type: "scatter3d",
+      marker: {
+        size: 5,
+        color: "rgb(23, 190, 207)",
+        opacity: 0.8,
+      },
+    },
+    {
+      x: [cordinate[0]],
+      y: [cordinate[1]],
+      z: [cordinate[2]],
+      mode: "markers",
+      type: "scatter3d",
+      marker: {
+        size: 5,
+        color: "red",
+        opacity: 0.8,
+      },
+    },
+  ];
+
+  const layout = {
+    margin: { l: 0, r: 0, b: 0, t: 0 },
+    scene: {
+      xaxis: { title: "X Axis" },
+      yaxis: { title: "Y Axis" },
+      zaxis: { title: "Z Axis" },
+    },
+  };
+
+  // Create the plot
+  const container = document.createElement("div");
+  Plotly.newPlot(container, trace, layout);
+  return container;
+};
